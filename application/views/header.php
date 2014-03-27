5c15ee636e0ccab3ca386d4d55655ece05072159
dlw <admin@4u4v.net>
2014-03-27 09:31:04 +0800

    提交已缓存的改动...

--- application/views/header.php
+++ application/views/header.php
@@ -3,4 +3,5 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title><?php echo $title;?></title>
+<link type="text/css" rel="stylesheet" href="<?php echo base_url()?>css/common.css"/>
 </head>
