83bccb241d8625a9b6ac657fd3210d43a5ea55e9
dlw <admin@4u4v.net>
2014-03-27 16:21:10 +0800

    数据库连接配置

--- application/config/database.php
+++ application/config/database.php
@@ -49,18 +49,18 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'root';
+$db['default']['password'] = '52mysql1314';
+$db['default']['database'] = 'my_cart';
 $db['default']['dbdriver'] = 'mysql';
-$db['default']['dbprefix'] = '';
+$db['default']['dbprefix'] = 'mc_';
 $db['default']['pconnect'] = TRUE;
 $db['default']['db_debug'] = TRUE;
 $db['default']['cache_on'] = FALSE;
 $db['default']['cachedir'] = '';
 $db['default']['char_set'] = 'utf8';
 $db['default']['dbcollat'] = 'utf8_general_ci';
-$db['default']['swap_pre'] = '';
+$db['default']['swap_pre'] = 'mc_';
 $db['default']['autoinit'] = TRUE;
 $db['default']['stricton'] = FALSE;
 
