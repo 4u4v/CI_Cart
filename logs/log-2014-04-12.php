4c108391e855724777ded905abf2fe37af284f12
dlw <admin@4u4v.net>
2014-04-14 10:33:19 +0800

    注册验证码

---
+++ logs/log-2014-04-12.php
@@ -0,0 +1,10 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2014-04-12 17:28:58 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead D:\phpStudy\WWW\CI_Cart\system\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2014-04-12 17:29:06 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead D:\phpStudy\WWW\CI_Cart\system\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2014-04-12 17:29:23 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead D:\phpStudy\WWW\CI_Cart\system\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2014-04-12 17:29:27 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead D:\phpStudy\WWW\CI_Cart\system\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2014-04-12 17:29:44 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead D:\phpStudy\WWW\CI_Cart\system\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2014-04-12 17:29:47 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead D:\phpStudy\WWW\CI_Cart\system\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2014-04-12 17:31:45 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead D:\phpStudy\WWW\CI_Cart\system\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2014-04-12 17:31:48 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead D:\phpStudy\WWW\CI_Cart\system\database\drivers\mysql\mysql_driver.php 91
